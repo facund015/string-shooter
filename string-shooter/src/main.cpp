4b1418b46dedb81cc700ceac3bb28c20d0563154
Ksmic <facundo.vecchi@outlook.com>
2021-09-19 20:36:14 -0500

    Update main.cpp

--- string-shooter/src/main.cpp
+++ string-shooter/src/main.cpp
@@ -1,9 +1,41 @@
 #include <Arduino.h>
 
+#define POTENTIOEMTER A0
+#define MOTORS D0
+#define BUTTON D1
+
+bool ON = false;
+int potency = 0;
+int buttonState;
+int pButtonState = HIGH;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  pinMode(MOTORS, OUTPUT);
+  pinMode(BUTTON, INPUT_PULLUP);
+
+  analogWrite(MOTORS, 0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  potency = analogRead(POTENTIOEMTER);
+  buttonState = digitalRead(BUTTON);
+
+  if (buttonState == LOW && pButtonState == HIGH){
+    if (ON) {
+      ON = false;
+    } else {
+      ON = true;
+    }
+  }
+
+  if (ON){
+    analogWrite(MOTORS, potency*5);
+  } else {
+    analogWrite(MOTORS, 0);
+  }
+
+  pButtonState = HIGH;
+  delay(500);
 }
